05547a29c3c6857baadf8fc4376b2dda5ad3b087
jae <wogud1514@naver.com>
2021-11-14 14:44:26 +0900

    nav 로그인 로그아웃 해결

--- src/main/java/ljh/commerce/commerce/configuration/SecurityConfig.java
+++ src/main/java/ljh/commerce/commerce/configuration/SecurityConfig.java
@@ -1,15 +1,21 @@
 package ljh.commerce.commerce.configuration;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
+
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     @Bean
     public BCryptPasswordEncoder encode() {
@@ -19,13 +25,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().
-                antMatchers("/shop/**","/cart/**").authenticated()
-                .antMatchers("/admin/**").authenticated()
-                .anyRequest().permitAll()
-                //.hasRole("ADMIN")
-                .and().formLogin().loginPage("/signin")
+        http.authorizeRequests()
+                .antMatchers("/", "/auth/**", "/js/**", "/css/**", "/shop/**", "/signup")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/signin").permitAll()
                 .loginProcessingUrl("/signin")
-                .defaultSuccessUrl("/home");
+                .defaultSuccessUrl("/"); // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인 해준다.
+//                .antMatchers("/","/shop/**","/signup",).permitAll()
+//                .antMatchers("/admin/**").hasRole("ADMIN")
+//                .anyRequest().authenticated()
+//                .and().formLogin().loginPage("/signin").permitAll()
+//                .failureUrl("/signin?error=true")
+//                .loginProcessingUrl("/signin")
+//                .defaultSuccessUrl("/home")
+//                .usernameParameter("username")
+//                .passwordParameter("password")
+//                .and()
+//                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/signin").invalidateHttpSession(true);
     }
 }
--- src/main/java/ljh/commerce/commerce/domain/user/UserRepository.java
+++ src/main/java/ljh/commerce/commerce/domain/user/UserRepository.java
@@ -4,5 +4,5 @@ import ljh.commerce.commerce.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Integer> {
-    User findByUsername(String Username);
+    User findByUsername(String username);
 }
